2a10828aecd9f72b5e55273df740008493bbd0af
devikakanyal <devikakanyal123@gmail.com>
2025-08-20 15:23:58 +0530

    Updates

---
+++ precache-manifest.3d2ab8dd67214b13b8639829b95a2818.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4506de151830847e8e73a05d34111899",
+    "url": "/MERN_INTERN/index.html"
+  },
+  {
+    "revision": "534d350af2723736a488",
+    "url": "/MERN_INTERN/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "a2ea34cfff3734792051",
+    "url": "/MERN_INTERN/static/css/main.5cd9dd9b.chunk.css"
+  },
+  {
+    "revision": "534d350af2723736a488",
+    "url": "/MERN_INTERN/static/js/2.c1231c01.chunk.js"
+  },
+  {
+    "revision": "9bf39d5c49166f2a866a40687348a682",
+    "url": "/MERN_INTERN/static/js/2.c1231c01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2ea34cfff3734792051",
+    "url": "/MERN_INTERN/static/js/main.0dda064e.chunk.js"
+  },
+  {
+    "revision": "072bcac608bc5e77c765",
+    "url": "/MERN_INTERN/static/js/runtime-main.01f9d623.js"
+  },
+  {
+    "revision": "f5641d1f1374b31ee8fe5228ff4dc91e",
+    "url": "/MERN_INTERN/static/media/resume.f5641d1f.pdf"
+  }
+]);
